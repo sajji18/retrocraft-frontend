c7c2dd3ad9a005585289f95ae815e7eb555828b3
Sajal Chauhan <sajalchauhan2004@gmail.com>
2023-12-21 02:46:37 +0530

    Freelancer Dashboard Job Posts Area Created, Frontend Edits

--- frontend/src/pages/FreelancerDashboard.jsx
+++ frontend/src/pages/FreelancerDashboard.jsx
@@ -6,11 +6,13 @@ import '../static/css/pages/FreelancerDashboard.css';
 const FreelancerDashboard = () => {
     const [userRole, setUserRole] = useState(localStorage.getItem("role"));
     const [token, setToken] = useState(localStorage.getItem("token"));
+    const [jobPosts, setJobPosts] = useState([]);
+    const [connectionRequests, setConnectionRequest] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
-        
-        axios.get('http://localhost:3000/details', {
+        axios
+        .get('http://localhost:3000/details', {
             headers: {
                 Authorization: `Bearer ${token}`
             }
@@ -29,6 +31,24 @@ const FreelancerDashboard = () => {
             navigate("/producer-dashboard");
         }
     }, []);
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:3000/freelancer/get-job-posts', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            console.log(response.data)
+            setJobPosts(response.data)
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+    }, [])
+
+    console.log(jobPosts)
 
     const handleLogout = () => {
         localStorage.clear();
@@ -55,6 +75,31 @@ const FreelancerDashboard = () => {
                     </div>
                     <div className='freelancer_navbar_right'>
                         <button className='freelancer_navbar_option_1' onClick={handleLogout}>Logout</button>
+                    </div>
+                </div>
+                <div className='freelancer_dash_main_area'>
+                    <div className='freelancer_dash_main_left_area'>
+                        <div className='freelancer_dash_main_left_head'>
+                            <span>Open Vacancies</span>
+                        </div>
+                        <div className='freelancer_dash_main_posts'>
+                            {
+                                jobPosts.map((job, index) => {
+                                    return (
+                                        <div className='freelancer_dash_job_card' key={job._id}>
+                                            <span>{job.title}</span>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                    
+                    <div className='freelancer_dash_main_right'>
+                        <div className='freelancer_dash_main_right_part1'>
+                            <span>Pending Requests</span>
+                            
+                        </div>
                     </div>
                 </div>
             </>
